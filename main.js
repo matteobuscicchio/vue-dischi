afebe23e10dea204a44f5122229b1cb446da1929
matteobuscicchio <matteoezio@me.com>
2021-01-11 18:31:10 +0100

    esercizio completo

---
+++ main.js
@@ -0,0 +1,37 @@
+// Attraverso una chiamata ajax all’Api di boolean 
+// avremo a disposizione una decina di dischi musicali. 
+// Servendoci di Vue JS stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. 
+// In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+
+
+// axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+// .then(function(index){
+//     app.eMailArray.push(index.data.response)
+// })
+
+let app = new Vue({ 
+	el: '#app',
+	
+    data: {
+		mDisks: [],
+	},
+
+	methods: {
+
+		eMailArrayGeneartor: function(){
+			axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+			.then(function(index){
+				console.log(index.data.response);
+				index.data.response.forEach(function(item) {
+					app.mDisks.push(item);
+				});
+			})
+        },
+	},
+
+	mounted(){
+        this.eMailArrayGeneartor();
+	}
+})
